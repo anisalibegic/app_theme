d81bd12fcf2484132098b39a373bcf51a6e16980
Anis Alibegić <anis.alibegic@gmail.com>
2020-11-11 14:40:54 +0100

    Update base_app_theme_data.dart
    Added accent and primary color.

--- lib/src/base_app_theme_data.dart
+++ lib/src/base_app_theme_data.dart
@@ -3,17 +3,23 @@ import 'package:flutter/services.dart';
 
 @immutable
 abstract class BaseAppThemeData {
+  final Color accentColor;
+  final Color primaryColor;
   final String fontFamily;
   final Brightness brightness;
   final MaterialColor swatch;
   final SystemUiOverlayStyle systemUiOverlayStyle;
 
   const BaseAppThemeData({
+    @required this.accentColor,
+    @required this.primaryColor,
     @required this.swatch,
     @required this.brightness,
     @required this.fontFamily,
     @required this.systemUiOverlayStyle,
-  })  : assert(swatch != null),
+  })  : assert(accentColor != null),
+        assert(primaryColor != null),
+        assert(swatch != null),
         assert(brightness != null),
         assert(fontFamily != null),
         assert(systemUiOverlayStyle != null);
